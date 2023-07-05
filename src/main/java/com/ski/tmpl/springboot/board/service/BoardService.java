fbfabb671c0970203b468bec973851cf5174b655
noisonnoiton <noisonnoiton@gmail.com>
2023-07-05 18:01:53 +0900

    remove unused

--- src/main/java/com/ski/tmpl/springboot/board/service/BoardService.java
+++ src/main/java/com/ski/tmpl/springboot/board/service/BoardService.java
@@ -1,7 +1,6 @@
 package com.ski.tmpl.springboot.board.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
